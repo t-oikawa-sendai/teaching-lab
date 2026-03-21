c77a18d6fba869c78ca0bc2db3d36e42e78c2f98
Oikawa1957 <oikawa@private.email.ne.jp>
2026-03-21 14:19:45 +0900

    feat: teaching-lab 初期構成（Java・Python環境確認）

---
+++ java/hello-java/Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java on MacBook Air!");
+        System.out.println("java.version = " + System.getProperty("java.version"));
+    }
+}
